2bc84027bea1aa20882b802fd1a6cb4f6a318e09
enrique <enrique@MacBook-Pro-de-enrique.local>
2025-12-03 17:04:19 +0100

    Fix forti.lab Content-Type and dns-attacker architecture

--- pki-badkeys/setup_pki.sh
+++ pki-badkeys/setup_pki.sh
@@ -85,6 +85,7 @@ server {
     ssl_ciphers DEFAULT@SECLEVEL=0;
     
     location / { 
+        add_header Content-Type text/html;
         return 200 '<h1>FortiGate Appliance</h1><p>System Online. Secure Connection Established.</p>'; 
     }
 }
